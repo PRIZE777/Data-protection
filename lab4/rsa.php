3a20778e1b09eaf18785b7662a79d6c54a2d3433
PRIZE777 <2latraine@mail.ru>
2017-12-10 17:54:36 +0300

    Пишем RSA

---
+++ lab4/rsa.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 10.12.2017
+ * Time: 17:28
+ */
+
+class rsa
+{
+
+    public static function prostoechislo(){
+
+    }
+}
+
+rsa::prostoechislo();
